5101ef422d7b131b143a4f669a44f1d148a4f130
Yomguithereal <guillaumeplique@gmail.com>
2021-09-29 15:37:09 +0200

    Modernizing legacy rules a bit

--- legacy.js
+++ legacy.js
@@ -13,6 +13,21 @@ module.exports = {
     mocha: true,
     jasmine: false
   },
-  globals: {},
+  globals: {
+    Set: true,
+    Map: true,
+    WeakMap: true,
+    Symbol: true,
+    ArrayBuffer: true,
+    Uint8Array: true,
+    Uint8ClampedArray: true,
+    Uint16Array: true,
+    Uint32Array: true,
+    Int8Array: true,
+    Int16Array: true,
+    Int32Array: true,
+    Float32Array: true,
+    Float64Array: true
+  },
   rules: {}
 };
